a893c970fa895a1ca3cc868d4f9601e9d15581f4
soundofautmn <62000745+soundofautumn@users.noreply.github.com>
2024-08-01 02:48:49 +0800

    feat: 拆分指令模块

---
+++ src/commands.ts
@@ -0,0 +1,51 @@
+import { Config } from "./config/settings";
+import { openRaidHandler, checkNowHandler, checkDetailHandler, exportHandler } from "./service/managerService";
+import { applyHandler, checkSelfHandler, contactLeaderHandler } from "./service/playerService";
+import { Context } from "koishi";
+
+export function commandSetup(ctx: Context, config: Config) {
+  // 指挥操作
+  ctx
+    .command('开团 <raid_name:string> <raid_time:date>', '开启一个新的raid团', {
+      authority: 4
+    })
+    .action(async (argv, raid_name: string, raid_time: Date) => {
+      return await openRaidHandler(ctx, config, argv, raid_name, raid_time);
+    });
+
+  ctx
+    .command('查看当前团', '查看当前所有开启中的团', {
+      authority: 4
+    })
+    .action(async argv => {
+      return await checkNowHandler(ctx, config, argv);
+    });
+
+  ctx
+    .command('查看报名状况', '查看单个团的报名详情列表', {
+      authority: 4
+    })
+    .action(async argv => {
+      return await checkDetailHandler(ctx, config, argv);
+    });
+
+  ctx
+    .command('导出报名状况 [encoding:string]', '导出单个团的报名详情列表', {
+      authority: 4
+    })
+    .action(async (argv, encoding: string) => {
+      return await exportHandler(ctx, config, argv, encoding);
+    });
+
+  // 报名者操作
+  ctx.command('报名').action(async argv => {
+    return await applyHandler(ctx, config, argv);
+  });
+
+  ctx.command('查看报名申请').action(async argv => {
+    return await checkSelfHandler(ctx, config, argv);
+  });
+
+  ctx.command('联系指挥').action(async argv => {
+    return await contactLeaderHandler(ctx, config, argv);
+  });}
--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,8 @@ import { Context } from 'koishi';
 import { Config } from './config/settings';
 import { dbSetup } from './constant/db';
 import {} from 'koishi-plugin-cron';
-import {
-  checkDetailHandler,
-  checkNowHandler,
-  exportHandler,
-  openRaidHandler
-} from './service/managerService';
-import {
-  applyHandler,
-  checkSelfHandler,
-  contactLeaderHandler
-} from './service/playerService';
 import { clearDict, noticeOneDayBefore } from './service/noticeService';
+import { commandSetup } from './commands';
 
 // 插件名
 export const name = 'ffxiv-raid-helper';
@@ -34,49 +24,5 @@ export function apply(ctx: Context, config: Config) {
   // 每天零点清理一次推送
   ctx.cron('* 0 * * *', clearDict);
 
-  // 指挥操作
-  ctx
-    .command('开团 <raid_name:string> <raid_time:date>', '开启一个新的raid团', {
-      authority: 4
-    })
-    .action(async (argv, raid_name: string, raid_time: Date) => {
-      return await openRaidHandler(ctx, config, argv, raid_name, raid_time);
-    });
-
-  ctx
-    .command('查看当前团', '查看当前所有开启中的团', {
-      authority: 4
-    })
-    .action(async argv => {
-      return await checkNowHandler(ctx, config, argv);
-    });
-
-  ctx
-    .command('查看报名状况', '查看单个团的报名详情列表', {
-      authority: 4
-    })
-    .action(async argv => {
-      return await checkDetailHandler(ctx, config, argv);
-    });
-
-  ctx
-    .command('导出报名状况 [encoding:string]', '导出单个团的报名详情列表', {
-      authority: 4
-    })
-    .action(async (argv, encoding: string) => {
-      return await exportHandler(ctx, config, argv, encoding);
-    });
-
-  // 报名者操作
-  ctx.command('报名').action(async argv => {
-    return await applyHandler(ctx, config, argv);
-  });
-
-  ctx.command('查看报名申请').action(async argv => {
-    return await checkSelfHandler(ctx, config, argv);
-  });
-
-  ctx.command('联系指挥').action(async argv => {
-    return await contactLeaderHandler(ctx, config, argv);
-  });
+  commandSetup(ctx, config);
 }
