c36ad491814ffbd3aba8086e5e45866d96b86821
soundofautmn <62000745+soundofautumn@users.noreply.github.com>
2024-08-01 14:05:01 +0800

    chore: 格式

--- src/constant/questionSheet.ts
+++ src/constant/questionSheet.ts
@@ -1,12 +1,12 @@
-import { Config } from "../config/settings";
-import { buildQuestion, QuestionDefine, QuestionType } from "./question";
+import { Config } from '../config/settings';
+import { buildQuestion, QuestionDefine, QuestionType } from './question';
 
 export const duties: Readonly<Record<string, ReadonlyArray<string>>> = {
-    "坦克": ['骑士', '战士', '黑骑', '绝枪'],
-    "治疗": ['白魔', '学者', '占星', '贤者'],
-    "近战": ['武僧', '龙骑', '忍者', '武士', '镰刀'],
-    "远敏": ['诗人', '机工', '武者'],
-    "法系": ['黑魔', '召唤', '赤魔']
+    '坦克': ['骑士', '战士', '黑骑', '绝枪'],
+    '治疗': ['白魔', '学者', '占星', '贤者'],
+    '近战': ['武僧', '龙骑', '忍者', '武士', '镰刀'],
+    '远敏': ['诗人', '机工', '武者'],
+    '法系': ['黑魔', '召唤', '赤魔']
 };
 
 export const getSheet = (server_name: string, config: Config) => {
@@ -19,46 +19,46 @@ export const getSheet = (server_name: string, config: Config) => {
         {
             label: '2',
             type: QuestionType.Boolean,
-            name: "是否初见",
+            name: '是否初见',
             content: '是否已经通关零式女王古殿？',
         },
         {
             label: '3',
             type: QuestionType.SignleChoice,
-            name: "所在服务器",
+            name: '所在服务器',
             content: '所在服务器',
-            construct_range: _ => new Map(config.server_name_map[server_name].map((server, idx) => [(idx + 1).toString(), server])),
+            construct_range: () => new Map(config.server_name_map[server_name].map((server, idx) => [(idx + 1).toString(), server])),
         },
         {
             label: '4',
             type: QuestionType.Text,
-            name: "角色姓名",
+            name: '角色姓名',
             content: '角色姓名'
         },
         {
             label: '5',
             type: QuestionType.Text,
-            name: "QQ(问卷填写)",
+            name: 'QQ(问卷填写)',
             content: 'QQ'
         },
         {
             label: '6',
             type: QuestionType.Boolean,
-            name: "是否接受调剂",
+            name: '是否接受调剂',
             content: '选择职业前请您注意，我们保证所有的开荒位置先到先得，但是如果您所选择的职能已经满员了，您是否愿意被调剂到其他职能？',
             answer_range: [1, 0]
         },
         {
             label: '7',
             type: QuestionType.SignleChoice,
-            name: "主选职能",
+            name: '主选职能',
             content: '职能-职业选择:职能',
-            construct_range: _ => new Map(Object.keys(duties).map((duty, idx) => [(idx + 1).toString(), duty])),
+            construct_range: () => new Map(Object.keys(duties).map((duty, idx) => [(idx + 1).toString(), duty])),
         },
         {
             label: '7-1',
             type: QuestionType.SignleChoice,
-            name: "主选职业",
+            name: '主选职业',
             content: '职能-职业选择:职业',
             construct_range: input => {
                 const duty = Object.keys(duties)[parseInt(input.get('7').answer) - 1];
@@ -68,14 +68,14 @@ export const getSheet = (server_name: string, config: Config) => {
         {
             label: '8',
             type: QuestionType.SignleChoice,
-            name: "次选职能",
+            name: '次选职能',
             content: '职能-职业选择:次选职能',
-            construct_range: _ => new Map(Object.keys(duties).map((duty, idx) => [(idx + 1).toString(), duty])),
+            construct_range: () => new Map(Object.keys(duties).map((duty, idx) => [(idx + 1).toString(), duty])),
         },
         {
             label: '8-1',
             type: QuestionType.SignleChoice,
-            name: "次选职业",
+            name: '次选职业',
             content: '职能-职业选择:次选职业',
             construct_range: input => {
                 const duty = Object.keys(duties)[parseInt(input.get('8').answer) - 1];
@@ -84,7 +84,7 @@ export const getSheet = (server_name: string, config: Config) => {
         },
         {
             label: '9',
-            name: "红色勋章层数",
+            name: '红色勋章层数',
             type: QuestionType.SignleChoice,
             content: '请您输入角色现有红色的战斗勋章层数:\n',
             answer_range: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -96,10 +96,10 @@ export const getSheet = (server_name: string, config: Config) => {
         // },
         {
             label: '11',
-            name: "留言",
+            name: '留言',
             type: QuestionType.Text,
             content: '还有什么废话是你想说给指挥听的吗？有的话请在下面畅所欲言吧~'
         }
     ];
     return questions.map(question => buildQuestion(question));
-}
+};
