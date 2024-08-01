12984fae2253a53402613350ddbdcbf3d1f0a4ea
aidon <silverblade999@outlook.com>
2024-08-01 19:32:44 +0800

    fix: :bug: 权限系统的api和文档似乎是反的

--- src/commands.ts
+++ src/commands.ts
@@ -17,38 +17,35 @@ export function commandSetup(ctx: Context, config: Config) {
   // 指挥操作
   ctx
     .command('开团 <raid_name:string> <raid_time:date>', '开启一个新的raid团', {
-      authority: 4
+      permissions: ['raid-helper:leader']
     })
     .action(async (argv, raid_name: string, raid_time: Date) => {
       return await openRaidHandler(ctx, config, argv, raid_name, raid_time);
     });
-  ctx.permissions.inherit('command:开团', ['raid-helper:leader']);
 
   ctx
-    .command('查看当前团', '查看当前所有开启中的团', { authority: 4 })
+    .command('查看当前团', '查看当前所有开启中的团', {
+      permissions: ['raid-helper:leader']
+    })
     .action(async argv => {
       return await checkNowHandler(ctx, config, argv);
     });
 
-  ctx.permissions.inherit('command:查看当前团', ['raid-helper:leader']);
-
   ctx
-    .command('查看报名状况', '查看单个团的报名详情列表', { authority: 4 })
+    .command('查看报名状况', '查看单个团的报名详情列表', {
+      permissions: ['raid-helper:leader']
+    })
     .action(async argv => {
       return await checkDetailHandler(ctx, config, argv);
     });
 
-  ctx.permissions.inherit('command:查看报名状况', ['raid-helper:leader']);
-
   ctx
     .command('导出报名状况 [encoding:string]', '导出单个团的报名详情列表', {
-      authority: 4
+      permissions: ['raid-helper:leader']
     })
     .action(async (argv, encoding: string) => {
       return await exportHandler(ctx, config, argv, encoding);
     });
-
-  ctx.permissions.inherit('command:导出报名状况', ['raid-helper:leader']);
 
   // 报名者操作
   ctx.command('报名').action(async argv => {
--- src/permissions.ts
+++ src/permissions.ts
@@ -3,13 +3,14 @@ import { Config } from './config/settings';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function permissionsSetup(ctx: Context, config: Config) {
-  ctx.permissions.inherit('raid-helper:leader', ['authority:2']);
-  ctx.permissions.inherit('raid-helper:admin', [
-    'authority:4',
-    'raid-helper:leader'
-  ]);
+  // 拥有admin权限视为拥有leader权限
+  ctx.permissions.inherit('raid-helper:leader', ['raid-helper:admin']);
 
   ctx.permissions.provide('raid-helper:admin', async (name, session) => {
     return session.onebot?.sender?.role === 'owner';
+  });
+
+  ctx.permissions.provide('raid-helper:leader', async (name, session) => {
+    return (await session.getUser()).authority >= 4;
   });
 }
