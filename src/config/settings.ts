7ac8573b0b1a130b319f6aa2c4641fd786531823
soundofautmn <62000745+soundofautumn@users.noreply.github.com>
2024-08-02 15:00:32 +0800

    refactor: 重构配置文件

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -4,14 +4,16 @@ export interface Config {
   message_interval: number;
   server_name_map: { [key: string]: string[] };
   server_group_map: { [key: string]: string[] };
-  group_config_maps: {
-    group_name: string;
-    platform: string;
-    leaders: { user_id: string; notice: boolean; admin: boolean }[];
-    groups: { group_id: string; notice: boolean }[];
-    servers: { name: string; children: string[] }[];
-    ignore_server: boolean;
-  }[];
+  group_config_map: {
+    [key: string]: {
+      platform: 'onebot' | 'sandbox';
+      admin: string;
+      leaders: { user_id: string; notice: boolean; admin: boolean }[];
+      groups: { group_id: string; notice: boolean }[];
+      server: string;
+      ignore_server: boolean;
+    };
+  };
 }
 
 export const Config: Schema<Config> = Schema.object({
@@ -21,44 +23,46 @@ export const Config: Schema<Config> = Schema.object({
     .step(1000)
     .description('每轮消息发送间隔，单位毫秒'),
   server_name_map: Schema.dict(
-    Schema.array(Schema.string()).role('table')
-  ).default({
-    陆行鸟: [
-      '红玉海',
-      '神意之地',
-      '拉诺西亚',
-      '幻影群岛',
-      '萌芽池',
-      '宇宙和音',
-      '沃仙曦染',
-      '晨曦王座'
-    ],
-    莫古力: [
-      '白金幻象',
-      '神拳痕',
-      '潮风亭',
-      '白银乡',
-      '旅人栈桥',
-      '拂晓之间',
-      '龙巢神殿',
-      '梦羽宝境'
-    ],
-    猫小胖: [
-      '紫水栈桥',
-      '静语庄园',
-      '延夏',
-      '摩杜纳',
-      '海猫茶屋',
-      '柔风海湾',
-      '琥珀原'
-    ],
-    豆豆柴: ['水晶塔', '银泪湖', '太阳海岸', '伊修加德', '红茶川']
-  }),
+    Schema.array(Schema.string()).role('table').description('小区的名字')
+  )
+    .default({
+      陆行鸟: [
+        '红玉海',
+        '神意之地',
+        '拉诺西亚',
+        '幻影群岛',
+        '萌芽池',
+        '宇宙和音',
+        '沃仙曦染',
+        '晨曦王座'
+      ],
+      莫古力: [
+        '白金幻象',
+        '神拳痕',
+        '潮风亭',
+        '白银乡',
+        '旅人栈桥',
+        '拂晓之间',
+        '龙巢神殿',
+        '梦羽宝境'
+      ],
+      猫小胖: [
+        '紫水栈桥',
+        '静语庄园',
+        '延夏',
+        '摩杜纳',
+        '海猫茶屋',
+        '柔风海湾',
+        '琥珀原'
+      ],
+      豆豆柴: ['水晶塔', '银泪湖', '太阳海岸', '伊修加德', '红茶川']
+    })
+    .collapse()
+    .description('服务器区服的对应关系'),
   server_group_map: Schema.dict(Schema.array(Schema.string()).role('table')),
-  group_config_maps: Schema.array(
+  group_config_map: Schema.dict(
     Schema.object({
-      group_name: Schema.string().description('团名'),
-      platform: Schema.string().description('机器人所在的platform'),
+      platform: Schema.union(['onebot', 'sandbox']).description('机器人平台'),
       admin: Schema.string().description('团长的user_id'),
       leaders: Schema.array(
         Schema.object({
@@ -77,15 +81,8 @@ export const Config: Schema<Config> = Schema.object({
       )
         .role('table')
         .description('相关的群列表'),
-      servers: Schema.array(
-        Schema.object({
-          name: Schema.string().description('服务器名，一般是大区'),
-          children: Schema.array(Schema.string())
-            .role('table')
-            .description('小区服务器名')
-        })
-      ).description('相关的区服列表'),
+      server: Schema.string().description('团所在服务器（大区）'),
       ignore_server: Schema.boolean().description('跨区支持，开启后不关注区服')
     })
-  )
+  ).description('团配置')
 });
