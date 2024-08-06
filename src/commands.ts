22807ea18de2addc73a1b9632b3245d0ce228b1e
soundofautmn <62000745+soundofautumn@users.noreply.github.com>
2024-08-07 03:51:04 +0800

    feat: 优化指令结构

--- src/commands.ts
+++ src/commands.ts
@@ -16,17 +16,22 @@ import {
 import { Context } from 'koishi';
 
 export function commandSetup(ctx: Context, config: Config) {
+  ctx.command('ffxiv-raid-helper', '最终幻想14高难组团管理助手');
   // 指挥操作
   ctx
-    .command('开团 <raid_name:string> <raid_time:date>', '开启一个新的raid团', {
-      permissions: ['raid-helper:leader']
-    })
+    .command(
+      'ffxiv-raid-helper/开团 <raid_name:string> <raid_time:date>',
+      '开启一个新的raid团',
+      {
+        permissions: ['raid-helper:leader']
+      }
+    )
     .action(async (argv, raid_name: string, raid_time: Date) => {
       return await openRaidHandler(ctx, config, argv, raid_name, raid_time);
     });
 
   ctx
-    .command('关闭报名', '提前关闭报名', {
+    .command('ffxiv-raid-helper/关闭报名', '提前关闭报名', {
       permissions: ['raid-helper:leader']
     })
     .action(async argv => {
@@ -34,7 +39,7 @@ export function commandSetup(ctx: Context, config: Config) {
     });
 
   ctx
-    .command('查看当前团', '查看当前所有开启中的团', {
+    .command('ffxiv-raid-helper/查看当前团', '查看当前所有开启中的团', {
       permissions: ['raid-helper:leader']
     })
     .action(async argv => {
@@ -42,23 +47,27 @@ export function commandSetup(ctx: Context, config: Config) {
     });
 
   ctx
-    .command('查看报名状况', '查看单个团的报名详情列表', {
+    .command('ffxiv-raid-helper/查看报名状况', '查看单个团的报名详情列表', {
       permissions: ['raid-helper:leader']
     })
+    .alias('查看报名详情')
+    .alias('查看报名情况')
     .action(async argv => {
       return await checkDetailHandler(ctx, config, argv);
     });
 
   ctx
-    .command('导出报名状况', '导出单个团的报名详情列表', {
+    .command('ffxiv-raid-helper/导出报名状况', '导出单个团的报名详情列表', {
       permissions: ['raid-helper:leader']
     })
+    .alias('导出报名详情')
+    .alias('导出报名情况')
     .action(async argv => {
       return await exportHandler(ctx, config, argv);
     });
 
   ctx
-    .command('推送消息到所有报名者', {
+    .command('ffxiv-raid-helper/推送消息', '向所有报名者推送消息', {
       permissions: ['raid-helper:leader']
     })
     .action(async argv => {
@@ -68,28 +77,28 @@ export function commandSetup(ctx: Context, config: Config) {
   // 报名者操作，仅私聊
   ctx
     .intersect(session => session.isDirect)
-    .command('报名')
+    .command('ffxiv-raid-helper/报名')
     .action(async argv => {
       return await applyHandler(ctx, config, argv);
     });
 
   ctx
     .intersect(session => session.isDirect)
-    .command('查看报名申请')
+    .command('ffxiv-raid-helper/查看报名')
     .action(async argv => {
       return await checkSelfHandler(ctx, config, argv);
     });
 
   ctx
     .intersect(session => session.isDirect)
-    .command('取消报名')
+    .command('ffxiv-raid-helper/取消报名')
     .action(async argv => {
       return await cancelSignupHandler(ctx, config, argv);
     });
 
   ctx
     .intersect(session => session.isDirect)
-    .command('联系指挥')
+    .command('ffxiv-raid-helper/联系指挥')
     .action(async argv => {
       return await contactLeaderHandler(ctx, config, argv);
     });
